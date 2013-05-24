b8aa221b1a13f60b4c99eddd08a26da98a8be09b
John Goodall <jgoodall@ornl.gov>
2013-05-24 12:19:10 -0400

    removed berkshelf gem, only the vagrant plugin is needed

--- init.sh
+++ init.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-# Install [berkshelf](http://berkshelf.com/) if it isnt installed already
-`which berks` || gem install berkshelf
-
-# Install cookbooks specified in Berksfile
-berks install
+# Install the Vagrant plugins (for Vagrant 1.1.0 and greater)
+vagrant plugin install vagrant-berkshelf
+vagrant plugin install vagrant-omnibus
 
 # Initialize the Vagrant vm
 vagrant up
