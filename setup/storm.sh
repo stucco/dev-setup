a96e055e820551c3e8eae3f235ca944bb7498ed5
John Goodall <jgoodall@gmail.com>
2014-01-14 13:56:45 -0500

    updated storm to 0.9.x

--- setup/storm.sh
+++ setup/storm.sh
@@ -8,7 +8,7 @@ VERSION=${1:-'0.8.2'}
 if [ ! -d /usr/local/storm-${VERSION} ]; then 
   echo "Installing Storm ${VERSION}..."
   cd /usr/local
-  sudo curl --silent -LO https://dl.dropbox.com/u/133901206/storm-${VERSION}.zip
+  sudo curl --silent -LO https://dl.dropboxusercontent.com/s/tqdpoif32gufapo/storm-${VERSION}.zip
   sudo unzip -qo storm-${VERSION}.zip
   sudo ln -s ../storm-${VERSION}/bin/storm bin/storm
   sudo rm -f storm-${VERSION}.zip
