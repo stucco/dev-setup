859a52e9ea3eeabad45a5068d0ebffc6efdb5836
John Goodall <jgoodall@gmail.com>
2014-01-15 11:14:06 -0500

    updated storm install script for 0.9.0.1

--- setup/storm.sh
+++ setup/storm.sh
@@ -3,15 +3,15 @@
 # Install [Storm](http://storm-project.net/)
 
 # Argument is the version to install, or default value
-VERSION=${1:-'0.8.2'}
+VERSION=${1:-'0.9.0.1'}
 
 if [ ! -d /usr/local/storm-${VERSION} ]; then 
   echo "Installing Storm ${VERSION}..."
   cd /usr/local
-  sudo curl --silent -LO https://dl.dropboxusercontent.com/s/tqdpoif32gufapo/storm-${VERSION}.zip
-  sudo unzip -qo storm-${VERSION}.zip
+  sudo curl --silent -L https://dl.dropboxusercontent.com/s/tqdpoif32gufapo/storm-${VERSION}.tar.gz -o storm.tgz
+  sudo tar xzfv storm.tgz
   sudo ln -s ../storm-${VERSION}/bin/storm bin/storm
-  sudo rm -f storm-${VERSION}.zip
+  sudo rm -f storm.tgz
   sudo chmod +x bin/storm
   echo "Storm has been installed."
 fi
