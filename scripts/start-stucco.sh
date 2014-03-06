34cd9830539cf67f3eca0f9034b68c4c9822727e
John Goodall <jgoodall@gmail.com>
2014-03-06 12:15:42 -0500

    run rt in the background so it doesnt hang the install

--- scripts/start-stucco.sh
+++ scripts/start-stucco.sh
@@ -8,7 +8,7 @@ RT_DIR=${STUCCO_HOME}/rt
 if [ -d ${RT_DIR} ]; then
   echo 'Starting rt storm topology...'
   cd ${RT_DIR}/stucco-topology
-  mvn exec:java
+  nohup mvn exec:java &
 else
   echo 'The rt repository is not available. Do a `git clone https://github.com/stucco/rt.git` into the main stucco project directory and it will be available in the virtual machine, mounted under /stucco'
 fi
