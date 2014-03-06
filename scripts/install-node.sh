9ce58db0a2131c989d6b662617347c502f91b025
John Goodall <jgoodall@gmail.com>
2014-03-05 21:59:04 -0500

    added -y to add-apt-repository for node install

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo apt-get install python-software-properties python g++ make -y
-sudo add-apt-repository ppa:chris-lea/node.js
+sudo add-apt-repository ppa:chris-lea/node.js -y
 sudo apt-get update
 sudo apt-get install nodejs -y
