1df16efff3ccbbdc30b3d10692bc7d6b4365cd33
John Goodall <jgoodall@gmail.com>
2014-03-05 21:18:43 -0500

    added -y to apt commands to work without interaction

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install python-software-properties python g++ make
+sudo apt-get install python-software-properties python g++ make -y
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
-sudo apt-get install nodejs
+sudo apt-get install nodejs -y
