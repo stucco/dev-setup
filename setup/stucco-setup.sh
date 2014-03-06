59966c6001b9e4fda3368e482c8790a13dbfe40a
John Goodall <jgoodall@gmail.com>
2014-03-05 20:43:18 -0500

    now getting data and correctly building stucco

--- setup/stucco-setup.sh
+++ setup/stucco-setup.sh
@@ -2,24 +2,31 @@
 
 ### Download the repositories
 
-export STUCCO_HOME=${DIR}
+STUCCO_HOME=/stucco
+STUCCO_DATA_DIR=${STUCCO_HOME}/data
 
-sudo mkdir $STUCCO_HOME
+sudo mkdir -p $STUCCO_HOME
 sudo chmod 4777 $STUCCO_HOME
 sudo chown vagrant:vagrant $STUCCO_HOME
 
+mkdir STUCCO_DATA_DIR
+
 cd $STUCCO_HOME
-repos="ontology config-loader rt collectors document-service"
+repos="ontology config-loader rt collectors document-service endogenous-data-uc1 get-exogenous-data"
 for repo in $repos; do
   IFS=" "
   echo "cloning ${repo}"
   git clone --recursive https://github.com/stucco/${repo}.git
 done
 
-# Additional setup
+# Download exogenous data and put in data dir
+cd $STUCCO_HOME/get-exogenous-data
+npm start
+mv data/* $STUCCO_DATA_DIR
 
-# [forever](https://github.com/nodejitsu/forever) for starting node.js daemons
-npm install -g forever --quiet
+# Move endogenous data into data dir
+cd $STUCCO_HOME/endogenous-data-uc1
+mv * $STUCCO_DATA_DIR
 
 # Load configuration into etcd
 cd $STUCCO_HOME/config-loader
