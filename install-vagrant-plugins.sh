2ac531c0cef0c9cbac18ab56d126ce093fb814ec
Mike Iannacone <euf@ornl.gov>
2014-10-09 11:44:50 -0400

    add snapshot plugin

--- install-vagrant-plugins.sh
+++ install-vagrant-plugins.sh
@@ -4,5 +4,6 @@
 vagrant plugin install vagrant-librarian-chef
 vagrant plugin install vagrant-omnibus
 vagrant plugin install vagrant-cachier
+vagrant plugin install vagrant-vbox-snapshot
 
 echo "Now you can start the VM with the command: vagrant up"
