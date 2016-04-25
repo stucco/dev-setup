06d95e0a1691ed599ff6a396af74bbe2212092a8
Mike Iannacone <euf@ornl.gov>
2016-04-25 15:49:12 -0400

    set env variables in /etc/environment and also in site.yml for ansible

--- scripts/base.sh
+++ scripts/base.sh
@@ -11,3 +11,9 @@ apt-get -yq install software-properties-common build-essential
 
 # Customize login message
 echo 'Welcome to your Stucco virtual machine.' > /etc/motd
+
+# Set environment variables that ansible roles will need
+echo 'STUCCO_DB_TYPE=ORIENTDB' >> /etc/environment
+echo 'STUCCO_DB_CONFIG="/stucco/graph-init/config/orientdb.yml"' >> /etc/environment
+echo 'STUCCO_DB_INDEX_CONFIG="/stucco/graph-init/config/stucco_orientdb_indexing.json"' >> /etc/environment
+#TODO: check if they were set previously
