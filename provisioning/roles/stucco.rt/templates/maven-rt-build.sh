163c6d01874900290d2d56204cc9da10be4c682b
Maria Vincent <Maria@marias-mbp.mshome.net>
2016-07-07 02:06:42 +0700

    rt role with postgres dependencies

---
+++ provisioning/roles/stucco.rt/templates/maven-rt-build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+echo "Checking out modules..."
+mvn --non-recursive scm:checkout -Dmodule.name=document-service-client-java
+mvn --non-recursive scm:checkout -Dmodule.name=STIXExtractors
+
+mvn --non-recursive scm:checkout -Dmodule.name=entity-extractor -DscmVersion=STIX -DscmVersionType=branch
+cd entity-extractor
+mvn -e clean install -Dmaven.test.skip=true
+cd ..
+
+mvn --non-recursive scm:checkout -Dmodule.name=relation-extractor -DscmVersion=STIX -DscmVersionType=branch
+cd relation-extractor
+mvn -e clean install -Dmaven.test.skip=true
+cd ..
+
+mvn -q --non-recursive scm:checkout -Dmodule.name=graph-db-connection -DscmVersion=PostgreSQL -DscmVersionType=branch
+cd graph-db-connection
+mvn -e clean install -Dmaven.test.skip=true
+cd ..
+
+mvn -q --non-recursive scm:checkout -Dmodule.name=graph-alignment -DscmVersion=loadSTIX -DscmVersionType=branch
+cd graph-alignment
+mvn -e clean install -Dmaven.test.skip=true
+cd ..
+
+
+echo "Building rt..."
+mvn -e clean install -Dmaven.test.skip=true
+cd streaming-processor
+mvn -e clean package -Dmaven.test.skip=true
+cd ..
