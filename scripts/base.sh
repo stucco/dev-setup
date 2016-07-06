a7bdf98c1e0d532c2238c92649867853bd6852bd
Maria Vincent <Maria@marias-mbp.mshome.net>
2016-07-07 02:19:46 +0700

    updated init scripts/config to work with postgres

--- scripts/base.sh
+++ scripts/base.sh
@@ -13,7 +13,7 @@ apt-get -yq install software-properties-common build-essential
 echo 'Welcome to your Stucco virtual machine.' > /etc/motd
 
 # Set environment variables that ansible roles will need
-echo 'STUCCO_DB_TYPE=ORIENTDB' >> /etc/environment
-echo 'STUCCO_DB_CONFIG="/stucco/graph-init/config/orientdb.yml"' >> /etc/environment
-echo 'STUCCO_DB_INDEX_CONFIG="/stucco/graph-init/config/stucco_orientdb_indexing.json"' >> /etc/environment
+echo 'STUCCO_DB_TYPE=POSTGRESQL' >> /etc/environment
+echo 'STUCCO_DB_CONFIG="/stucco/rt/graph-db-connection/config/postgresql.yml"' >> /etc/environment
+echo 'STUCCO_DB_INDEX_CONFIG=""' >> /etc/environment
 #TODO: check if they were set previously
